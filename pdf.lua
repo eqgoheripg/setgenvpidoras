15b965e45682e0fa218595be76acd10ae6b3517a
eqgoheripg <kdhdhdjskshsv@gmail.com>
2026-08-23 23:33:21 +0300

    Update pdf.lua

--- pdf.lua
+++ pdf.lua
@@ -7,7 +7,7 @@ local player = Players.LocalPlayer or Players.PlayerAdded:Wait()
 
 local GamesHub = {
     [1742264997] = "https://api.jnkie.com/api/v1/luascripts/public/8c99f152b4cc52196240e9c79b5a4c18c19674976ad30c5846911cce5d49e6f7/download", -- SCP Roleplay
-    [7239319209] = "loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/723aac2fcb8192264bef1dd55fc537c0faa2633e122f6960c9b2cb77d00c01e3/download"))()", -- ohio
+    [7239319209] = "https://api.jnkie.com/api/v1/luascripts/public/723aac2fcb8192264bef1dd55fc537c0faa2633e122f6960c9b2cb77d00c01e3/download", -- ohio
 }
 
 local currentGameId  = game.GameId
